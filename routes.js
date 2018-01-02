c0e17cc8efb9373fee87eba62a3dc7007801fc5d
Jeremy Leu <jeremyleu@yahoo.com>
2018-01-01 19:15:07 -0800

    added scss

--- routes.js
+++ routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-// Define the about route
 router.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
 
 module.exports = router;
