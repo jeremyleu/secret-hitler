2c9b7e3689d66ef3cf26f1e78adb02b228eb52b4
Jeremy Leu <jeremyleu@yahoo.com>
2018-01-01 22:07:23 -0800

    fix webpack config prod

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -186,9 +186,6 @@ module.exports = {
                       },
                     },
                     {
-                      loader: require.resolve('sass-loader'),
-                    },
-                    {
                       loader: require.resolve('postcss-loader'),
                       options: {
                         // Necessary for external CSS imports to work
@@ -234,6 +231,9 @@ module.exports = {
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
                     },
                     {
                       loader: require.resolve('postcss-loader'),
