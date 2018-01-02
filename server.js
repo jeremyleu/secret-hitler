fb22e0f4f6cb908db67f0e153d7ebfb9c7ce672a
Jeremy Leu <jeremyleu@yahoo.com>
2018-01-01 16:45:42 -0800

    revert back to yarn

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
