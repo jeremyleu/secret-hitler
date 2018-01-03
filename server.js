fe8365b3da1640954b3e0a45173f757f7f12a18d
Jeremy Leu <jeremyleu@yahoo.com>
2018-01-03 02:46:39 -0800

    add heroku redis prod

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(require('./routes'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const client = redis.createClient();
+const client = process.env.REDIS_URL ? redis.createClient(process.env.REDIS_URL) :
+  redis.createClient();
 
 client.on('connect', () => {
   console.log('connected to redis');
