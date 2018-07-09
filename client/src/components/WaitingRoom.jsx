27987913a8ab41cb9df6a6b422ce9bb4837ce6ad
Jeremy Leu <jeremyleu@yahoo.com>
2018-07-08 23:23:15 -0700

    linting changes

--- client/src/components/WaitingRoom.jsx
+++ client/src/components/WaitingRoom.jsx
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import './WaitingRoom.scss';
-import socket from '../socket';
-import { updatePlayers } from '../actions';
+import "./WaitingRoom.scss";
+import socket from "../socket";
+import { updatePlayers } from "../actions";
 
 class WaitingRoom extends Component {
   constructor(props) {
     super(props);
     const { dispatch } = this.props;
-    socket.on('playerJoinSuccess', (players) => {
+    socket.on("playerJoinSuccess", players => {
       dispatch(updatePlayers(players));
     });
   }
 
   render() {
     const { players, name } = this.props;
+    console.log(players);
     return (
       <div className="waiting-room">
         <ul className="player-list list-group">
           {players.map(playerName => (
-            <li className={`player-list-item list-group-item ${name === playerName && 'self'}`} key={`${playerName}-list-item`}>
+            <li
+              className={`player-list-item list-group-item ${name ===
+                playerName && "self"}`}
+              key={`${playerName}-list-item`}
+            >
               {playerName}
             </li>
           ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps(state) {
-  return {...state};
+  return { ...state };
 }
 
 export default connect(mapStateToProps)(WaitingRoom);
